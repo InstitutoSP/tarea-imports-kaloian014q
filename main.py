137da39ed69a54b4792152c848c1af2e3a2cd9d9
kaloian014q <gassibegabrovskakaloian-sec@instituto-sanpablo.com>
2024-04-17 22:00:47 -0300

    cambios terminados

---
+++ main.py
@@ -0,0 +1,29 @@
+import funciones
+
+while True:
+    print("Seleccione la operación:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
+
+    opcion = input("Ingrese el número de la operación deseada: ")
+
+    if opcion == '5':
+        print("Saliendo de la calculadora.")
+        break
+
+    num1 = int(input("Ingrese el primer número: "))
+    num2 = int (input("Ingrese el segundo número: "))
+
+    if opcion == '1'=funciones:
+        print("El resultado de la suma es:", suma(num1, num2))
+    elif opcion == '2'=funciones:
+        print("El resultado de la resta es:", resta(num1, num2))
+    elif opcion == '3'=funciones:
+        print("El resultado de la multiplicación es:", multiplicacion(num1, num2))
+    elif opcion == '4'=funciones:
+        print("El resultado de la división es:", division(num1, num2))
+    else:
+        print("Opción no válida. Por favor, seleccione una opción válida.")
